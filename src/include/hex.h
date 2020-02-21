df25b23bee2d7aef129e9231ede22a20da214a69
novrain <novrainn@gmail.com>
2020-02-22 01:06:48 +0800

    :bug: need to decode(encode) directly from(to) hex

--- src/include/hex.h
+++ src/include/hex.h
@@ -9,11 +9,21 @@ extern "C"
 #endif
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
     size_t bin2hex(const uint8_t *bin, size_t binLen, uint8_t *hex, size_t hexLen);
     size_t hex2bin(const uint8_t *hex, size_t hexLen, uint8_t *bin, size_t binLen);
+
+    bool hexToUInt8(const uint8_t *hex, uint8_t *val);
+    bool hexToBeUInt16(const uint8_t *hex, uint16_t *val);
+    bool hexToBeUInt32(const uint8_t *hex, uint32_t *val);
+    bool hexToBeUInt64(const uint8_t *hex, uint64_t *val);
+
+    bool hexToLeUInt16(const uint8_t *hex, uint16_t *val);
+    bool hexToLeUInt32(const uint8_t *hex, uint32_t *val);
+    bool hexToLeUInt64(const uint8_t *hex, uint64_t *val);
 
 #ifdef __cplusplus
 }
