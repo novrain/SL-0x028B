d7f5fd8d07e4d1396dd844a77596475245a3b1d5
novrain <novrainn@gmail.com>
2020-06-06 10:51:26 +0800

    :rocket: packet creator factory init.

---
+++ test/TestPacketCreator.cpp
@@ -0,0 +1,25 @@
+#include "gtest/gtest.h"
+
+#include "common/class.h"
+#include "packet_creator.h"
+
+GTEST_TEST(Packet_creator, factoryLoad)
+{
+    PacketCreatorFactory *factory = NewInstance(PacketCreatorFactory);
+    PacketCreatorFactory_ctor(factory, 10);
+    PacketCreatorFactory_loadDirectory(factory, "./schemas/factory");
+    ASSERT_TRUE(PacketCreatorFactory_count(factory) == 1);
+    PacketCreator *c = PacketCreatorFactory_getPacketCreator(factory, "test_schema");
+    ASSERT_TRUE(c != NULL);
+    ASSERT_TRUE(strcmp(PacketCreator_schemaName(c), "test_schema") == 0);
+    c = PacketCreatorFactory_loadFile(factory, "./schemas/factory/test_schema.json");
+    ASSERT_TRUE(strcmp(PacketCreator_schemaName(c), "test_schema") == 0);
+    ASSERT_TRUE(c != NULL);
+    ASSERT_TRUE(PacketCreatorFactory_count(factory) == 1);
+}
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
