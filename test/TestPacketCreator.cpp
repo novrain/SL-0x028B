6fc9dc177f84ba922afdd635810c74303c12cee0
novrain <novrainn@gmail.com>
2020-06-12 09:32:09 +0800

    :hammer: move json to file.

--- test/TestPacketCreator.cpp
+++ test/TestPacketCreator.cpp
@@ -1,35 +1,12 @@
 #include "gtest/gtest.h"
 
 #include "common/class.h"
+#include "cJSON/cJSON_Helper.h"
 #include "packet_creator.h"
 
 GTEST_TEST(Packet_creator, timingReport)
 {
-    cJSON *data = cJSON_Parse("{                            "
-                              "    \"fcode\":\"32\",        "
-                              "    \"observetime\":\"2006101700\",        "
-                              "    \"direction\":0,         "
-                              "    \"elements\":[           "
-                              "        {                    "
-                              "            \"t\":\"number\","
-                              "            \"id\":\"26\",   "
-                              "            \"vt\":\"11\",   "
-                              "            \"v\":4          "
-                              "        },                   "
-                              "        {                    "
-                              "            \"t\":\"number\","
-                              "            \"id\":\"39\",   "
-                              "            \"vt\":\"12\",   "
-                              "            \"v\":13.2       "
-                              "        },                   "
-                              "        {                    "
-                              "            \"t\":\"number\","
-                              "            \"id\":\"38\",   "
-                              "            \"vt\":\"12\",   "
-                              "            \"v\":0.06       "
-                              "        }                    "
-                              "    ]                        "
-                              "}                            ");
+    cJSON *data = cJSON_FromFile("./jsontopkg/timing_report.json");
     Package *pkg = createPackage(data);
     ASSERT_TRUE(pkg != NULL);
     ASSERT_EQ(pkg->head.direction, Up);
@@ -58,34 +35,7 @@ GTEST_TEST(Packet_creator, timingReport)
 
 GTEST_TEST(Packet_creator, evenTimeReport)
 {
-    cJSON *data = cJSON_Parse("{                                         "
-                              "    \"fcode\":\"32\",                     "
-                              "    \"observetime\":\"2006101700\",       "
-                              "    \"direction\":0,                      "
-                              "    \"elements\":[                        "
-                              "        {                                 "
-                              "            \"t\":\"time_step_code\",     "
-                              "            \"step\":\"000005\",          "
-                              "            \"id\":\"39\",                "
-                              "            \"vt\":\"12\",                "
-                              "            \"v\":[                       "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                0                         "
-                              "            ]                             "
-                              "        }                                 "
-                              "    ]                                     "
-                              "}                                         ");
+    cJSON *data = cJSON_FromFile("./jsontopkg/even_time_report.json");
     Package *pkg = createPackage(data);
     ASSERT_TRUE(pkg != NULL);
     ASSERT_EQ(pkg->head.direction, Up);
@@ -100,30 +50,7 @@ GTEST_TEST(Packet_creator, evenTimeReport)
 
 GTEST_TEST(Packet_creator, rain5Min)
 {
-    cJSON *data = cJSON_Parse("{                                         "
-                              "    \"fcode\":\"34\",                     "
-                              "    \"observetime\":\"2006101700\",       "
-                              "    \"direction\":0,                      "
-                              "    \"elements\":[                        "
-                              "        {                                 "
-                              "            \"t\":\"rain_hour_5min\",     "
-                              "            \"v\":[                       "
-                              "                1.5,                      "
-                              "                1.5,                      "
-                              "                1.5,                      "
-                              "                1.5,                      "
-                              "                1.5,                      "
-                              "                1.5,                      "
-                              "                1.5,                      "
-                              "                1.5,                      "
-                              "                1.5,                      "
-                              "                1.5,                      "
-                              "                1.5,                      "
-                              "                1.5                       "
-                              "            ]                             "
-                              "        }                                 "
-                              "    ]                                     "
-                              "}                                         ");
+    cJSON *data = cJSON_FromFile("./jsontopkg/rain_5_min.json");
     Package *pkg = createPackage(data);
     ASSERT_TRUE(pkg != NULL);
     ASSERT_EQ(pkg->head.direction, Up);
@@ -141,77 +68,7 @@ GTEST_TEST(Packet_creator, rain5Min)
 
 GTEST_TEST(Packet_creator, water5Min)
 {
-    cJSON *data = cJSON_Parse("{                                         "
-                              "    \"fcode\":\"34\",                     "
-                              "    \"observetime\":\"2006101700\",       "
-                              "    \"direction\":0,                      "
-                              "    \"elements\":[                        "
-                              "        {                                 "
-                              "            \"t\":\"water_hour_5min\",    "
-                              "            \"id\":\"F5\",                "
-                              "            \"v\":[                       "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.25,                     "
-                              "                1.5,                      "
-                              "                1.5                       "
-                              "            ]                             "
-                              "        }                                 "
-                              "    ]                                     "
-                              "}                                         ");
-    Package *pkg = createPackage(data);
-    ASSERT_TRUE(pkg != NULL);
-    ASSERT_EQ(pkg->head.direction, Up);
-    ASSERT_EQ(pkg->head.funcCode, 0x34);
-    LinkMessage *linkMsg = (LinkMessage *)pkg;
-    // ASSERT_EQ(12, LinkMessage_ElementsSize(linkMsg));
-    RelativeWaterLevelElement *el = (RelativeWaterLevelElement *)LinkMessage_ElementAt(linkMsg, 0);
-    ASSERT_EQ(BB_Size(el->buff), 24);
-    float fv = 0;
-    ASSERT_EQ(2, RelativeWaterLevelElement_GetValueAt(el, 0, &fv));
-    ASSERT_EQ(1.15f, fv);
-    ASSERT_EQ(2, RelativeWaterLevelElement_GetValueAt(el, 11, &fv));
-    ASSERT_EQ(1.5f, fv);
-    ASSERT_EQ(2, RelativeWaterLevelElement_GetValueAt(el, 9, &fv));
-    ASSERT_EQ(1.25f, fv);
-    pkg->vptr->dtor(pkg);
-    DelInstance(pkg);
-}
-
-GTEST_TEST(Packet_creator, water5Min)
-{
-    cJSON *data = cJSON_Parse("{                                         "
-                              "    \"fcode\":\"34\",                     "
-                              "    \"observetime\":\"2006101700\",       "
-                              "    \"direction\":0,                      "
-                              "    \"elements\":[                        "
-                              "        {                                 "
-                              "            \"t\":\"water_hour_5min\",    "
-                              "            \"id\":\"F5\",                "
-                              "            \"v\":[                       "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.15,                     "
-                              "                1.25,                     "
-                              "                1.5,                      "
-                              "                1.5                       "
-                              "            ]                             "
-                              "        }                                 "
-                              "    ]                                     "
-                              "}                                         ");
+    cJSON *data = cJSON_FromFile("./jsontopkg/water_5_min.json");
     Package *pkg = createPackage(data);
     ASSERT_TRUE(pkg != NULL);
     ASSERT_EQ(pkg->head.direction, Up);
