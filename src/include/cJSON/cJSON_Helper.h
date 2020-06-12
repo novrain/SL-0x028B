a9cecc851b00f7c7fd7d81959699085e5f248aa6
novrain <novrainn@gmail.com>
2020-06-12 09:20:29 +0800

    :hammer: change json access to case insensitive

--- src/include/cJSON/cJSON_Helper.h
+++ src/include/cJSON/cJSON_Helper.h
@@ -8,41 +8,41 @@ extern "C"
 
 #include "cJSON/cJSON.h"
 
-#define cJSON_GET_VALUE(target, t, jParent, vField, defaultValue)                 \
-    cJSON *target##InJSON = cJSON_GetObjectItemCaseSensitive((jParent), #target); \
-    t target = defaultValue;                                                      \
-    if (target##InJSON != NULL)                                                   \
-    {                                                                             \
-        (target) = (t)target##InJSON->vField;                                     \
+#define cJSON_GET_VALUE(target, t, jParent, vField, defaultValue)    \
+    cJSON *target##InJSON = cJSON_GetObjectItem((jParent), #target); \
+    t target = defaultValue;                                         \
+    if (target##InJSON != NULL)                                      \
+    {                                                                \
+        (target) = (t)target##InJSON->vField;                        \
     }
 
-#define cJSON_GET_NUMBER(target, t, jParent, defaultValue, base)                  \
-    cJSON *target##InJSON = cJSON_GetObjectItemCaseSensitive((jParent), #target); \
-    t target = defaultValue;                                                      \
-    if (target##InJSON != NULL)                                                   \
-    {                                                                             \
-        if (target##InJSON->type == cJSON_Number)                                 \
-        {                                                                         \
-            (target) = (t)target##InJSON->valuedouble;                            \
-        }                                                                         \
-        else if (target##InJSON->type == cJSON_String)                            \
-        {                                                                         \
-            (target) = (t)strtol(target##InJSON->valuestring, NULL, (base));      \
-        }                                                                         \
+#define cJSON_GET_NUMBER(target, t, jParent, defaultValue, base)             \
+    cJSON *target##InJSON = cJSON_GetObjectItem((jParent), #target);         \
+    t target = defaultValue;                                                 \
+    if (target##InJSON != NULL)                                              \
+    {                                                                        \
+        if (target##InJSON->type == cJSON_Number)                            \
+        {                                                                    \
+            (target) = (t)target##InJSON->valuedouble;                       \
+        }                                                                    \
+        else if (target##InJSON->type == cJSON_String)                       \
+        {                                                                    \
+            (target) = (t)strtol(target##InJSON->valuestring, NULL, (base)); \
+        }                                                                    \
     }
 
-#define cJSON_COPY_VALUE(target, jField, jParent, vField)                   \
-    cJSON *(jField) = cJSON_GetObjectItemCaseSensitive((jParent), #jField); \
-    if ((jField) != NULL)                                                   \
-    {                                                                       \
-        (target) = (jField)->vField;                                        \
+#define cJSON_COPY_VALUE(target, jField, jParent, vField)      \
+    cJSON *(jField) = cJSON_GetObjectItem((jParent), #jField); \
+    if ((jField) != NULL)                                      \
+    {                                                          \
+        (target) = (jField)->vField;                           \
     }
 
-#define cJSON_COPY_VALUE_CAST(target, jField, jParent, vField, t)           \
-    cJSON *(jField) = cJSON_GetObjectItemCaseSensitive((jParent), #jField); \
-    if ((jField) != NULL)                                                   \
-    {                                                                       \
-        (target) = (t)(jField)->vField;                                     \
+#define cJSON_COPY_VALUE_CAST(target, jField, jParent, vField, t) \
+    cJSON *(jField) = cJSON_GetObjectItem((jParent), #jField);    \
+    if ((jField) != NULL)                                         \
+    {                                                             \
+        (target) = (t)(jField)->vField;                           \
     }
 
     cJSON *cJSON_FromFile(char const *const file);
